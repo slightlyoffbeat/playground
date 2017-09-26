f1c164758ecf1e7659d34faee6acf821a12608bb
Dan Brown <slightlyoffbeat@gmail.com>
2017-09-25 21:03:12 -0700

    testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.</p>
-  </div>
-);
+class NotFoundPage extends React.Component {
+  componentDidMount() {
+    console.log('1');
+    if (window.location.pathname.indexOf('/&') === 0) {
+      console.log('2');
+      window.location.href = window.location.href.replace('/&', '/?');
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist!</p>
+      </div>
+    );
+  }
+}
+
 
 export default NotFoundPage;
